248ed09efef28458275df15a0474f49f770b2026
David-Zg <branzevallos21@gmail.com>
2024-09-09 00:37:01 -0500

    Se agregaron nuevas tablas

--- Entregables/dataBase/dsrp_prestamos_financier.sql
+++ Entregables/dataBase/dsrp_prestamos_financier.sql
@@ -2,7 +2,7 @@
     GO
 
     USE dsrp_prestamos_financieros;
-    GO
+    
 
     CREATE TABLE personas_naturales (
         id INT PRIMARY KEY IDENTITY(1,1),
@@ -15,3 +15,36 @@
         email VARCHAR(50) NOT NULL
     );
     GO
+
+    -- Personas juridicas
+    CREATE TABLE personas_juridicas(
+        id INT PRIMARY KEY IDENTITY(1,1),
+        numero_documento VARCHAR(15) UNIQUE NOT NULL,
+        razpn_social VARCHAR(100) NOT NULL,
+        domicilio_fiscal NVARCHAR(1000) NOT NULL,
+        telefono VARCHAR(15) NOT NULL,
+        email VARCHAR(50) NOT NULL
+    );
+    GO
+
+    -- Clientes
+    CREATE TABLE clientes(
+        id INT PRIMARY KEY IDENTITY(1,1),
+        persona_id INT NOT NULL,
+        tipo_cliente VARCHAR(100) NOT NULL,
+        fecha_registro DATETIME DEFAULT getdate() NOT NULL,
+    );
+    GO
+
+    -- tipos de prestamos
+    CREATE TABLE tipos_prestamos(
+        id INT PRIMARY KEY IDENTITY(1,1),
+        nombre VARCHAR(100) NOT NULL,
+        estado BIT NOT NULL,
+        descripcion VARCHAR(1000),
+    );
+    GO
+
+    --EXEC sp_help clientes;
+    -- 
+    --SELECT GETDATE();
